bb2d3e42190c00710ea9ab2b7feff2e5b4e082b6
Steve Erickson <rimact@mail.com>
2021-02-07 21:54:57 +0000

    changes from lab

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+    var interest=(principal*years*rate)/100;
+    var d = new Date();
+    var y = d.getFullYear();
+    year=y+(years*1);
+    spans=document.getElementsByTagName("span");
+    spans=document.getElementsByTagName("span");
+        spans[1].innerHTML= "If you deposit "+principal+","+
+"at an interest rate of "+spans[0].innerHTML+"."+
+"You will receive an amount of "+interest+","+
+"in the year "+year;
 }
-        
+function readRange()
+{
+rate=document.getElementById("rate").value;
+spans=document.getElementsByTagName("span");
+spans[0].innerHTML=rate+"%";
+}
